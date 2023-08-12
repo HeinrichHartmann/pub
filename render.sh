e60b329bc313a29acbedabb34e6ae40400317887
Heinrich Hartmann <derhein@gmail.com>
2023-08-12 12:03:07 +0000

    .

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 python -m nbconvert "$1" --output "$2" \
     --to HTML \
     --TemplateExporter.extra_template_basedirs=$(pwd) \
     --template my-jupyter-template \
     --embed-images
-    
